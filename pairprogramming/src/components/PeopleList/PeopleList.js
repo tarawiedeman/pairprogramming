cf39654234a0582e9908bb78b2db01965122d240
benmartin6741 <bmartin6741@gmail.com>
2023-04-20 14:17:39 -0400

    fix server issues

--- pairprogramming/src/components/PeopleList/PeopleList.js
+++ pairprogramming/src/components/PeopleList/PeopleList.js
@@ -1,26 +1,32 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
 import PersonCard from '../PersonCard/PersonCard';
-let baseURL = "http://localhost"
-let port = "8080"
+let baseURL = "http://localhost";
+let port = "8080";
 let peopleArr = [];
 
+
+
 function PeopleList() {
+    let [loaded, setLoaded] = useState(false);
     useEffect(() => {
         axios.get(`${baseURL}:${port}/people`)
         .then(response => {
-            peopleArr= response.data;
+            peopleArr = JSON.parse(response.data);
+            setLoaded(true);
+        })
+        .catch(error => {
+            console.log(error);
         })
     }, [])
 
-    if (peopleArr === []) {
+    if (!loaded) {
         return <div>loading...</div>
     }
 
     return <div>
-        {peopleArr.map(person =>
-            <PersonCard key={person.id} id={person.id} name={person.name} headshot={person.headshot} applyingFor={person.applyingFor}/>
-            )}
+        {console.log(peopleArr)}
+        {peopleArr.map(person =><PersonCard key={person.id} id={person.id} name={person.name} headshot={person.headshot} applyingFor={person.applyingFor}/>)}
     </div>
 }
 export default PeopleList;
